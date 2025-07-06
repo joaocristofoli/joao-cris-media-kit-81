98d714f80d2956c1129534427fd86f364b24fba7
joaocristofoli <156140353+joaocristofoli@users.noreply.github.com>
2025-07-06 19:35:01 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/joao-cris-media-kit/",
   server: {
     host: "::",
     port: 8080,
